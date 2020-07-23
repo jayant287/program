a6af2a7b0f1e0a83381f144078b73dcba6c52346
jayant287 <67455822+jayant287@users.noreply.github.com>
2020-07-23 18:08:17 +0530

    commit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-module.exports = require('./src/PathFinding');
+
+module.exports = require("./src/PathFinding");
+
+var express = require("express");
+
+var path = require("path");
+
+var app = express();
+
+var bodyParser = require("body-parser");
+
+app.use((req, res, next) => {
+  bodyParser.json()(req, res, (err) => {
+    if (err) {
+      //console.error(err);
+
+      console.log("Bad JSON body ");
+
+      return res.sendStatus(400); // Bad request
+    }
+
+    next();
+  });
+});
+
+app.use(express.static(path.join(__dirname, "visual")));
+
+const port = process.env.PORT || 8080;
+
+app.listen(port);
+
+console.log("App is listening on port " + port);
+
+app.get("/", function (req, res) {
+  console.log("You have reached an unauthorized site");
+
+  res.status(403).json({ error: "You have reached an unauthorized site" });
+});
